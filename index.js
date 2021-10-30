ee7165748377cccbfa47dd034fec06816e63926e
Md. Rafiuzzaman Bhuiyan Afridi <afridirk10@gmail.com>
2021-10-30 21:43:45 +0600

    updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const CORS = require('cors');
 require('dotenv').config()
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 app.use(CORS());
 app.use(express.json());
 
